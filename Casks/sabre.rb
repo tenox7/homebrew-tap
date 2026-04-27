66be268c841aa0b45a6c38065165945b7b598e16
Antoni Sawicki <as@tenoware.com>
2026-04-26 17:09:08 -0700

    sabre: bump to 0.3.4

--- Casks/sabre.rb
+++ Casks/sabre.rb
@@ -1,6 +1,6 @@
 cask "sabre" do
-  version "0.3.3"
-  sha256 "9b82f521fd3574eb415f257f5e4197f11a637166dca553ad380798703f23be92"
+  version "0.3.4"
+  sha256 "d092c88ecbcc834b2273cdd7983d1f4f0815ee6276daa67e78ffe61371518aaf"
 
   url "https://github.com/tenox7/sabreOSX/releases/download/v#{version}/Sabre.dmg"
   name "Sabre"
