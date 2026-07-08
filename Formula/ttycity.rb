48a4157cd6d1e7ef7c29dda3d39c2ed9048161cf
Antoni Sawicki <as@tenoware.com>
2026-07-07 23:54:37 -0700

    ttycity: bump to 1.0.1

--- Formula/ttycity.rb
+++ Formula/ttycity.rb
@@ -1,8 +1,8 @@
 class Ttycity < Formula
   desc "Micropolis (SimCity) for the terminal, using ncurses"
   homepage "https://github.com/tenox7/ttycity"
-  url "https://github.com/tenox7/ttycity/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "3952f2e131c39e97716947007c982bb84e69d9bcd2de681f616b5a0f58618637"
+  url "https://github.com/tenox7/ttycity/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "fa2f51216055bf2a827ed2f3ccd6dc1f712707d5e5d1c9306398a8d9dfa769cb"
   license "GPL-3.0-or-later"
 
   def install
